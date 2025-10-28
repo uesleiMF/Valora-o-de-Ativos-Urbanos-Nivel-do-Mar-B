5fc3a752e86e94213a67786f20b1a1b90ee6475f
Jose Ueslei De Moraes Ferreira <joseueslei27@gmail.com>
2025-10-27 21:16:07 -0300

    g

--- models/Imovel.js
+++ models/Imovel.js
@@ -1,16 +1,18 @@
 import mongoose from "mongoose";
 
+
 const imovelSchema = new mongoose.Schema({
   titulo: { type: String, required: true },
   endereco: { type: String, required: true },
-  latitude: { type: Number, required: true },
-  longitude: { type: Number, required: true },
+  latitude: { type: String, required: true }, 
+  longitude: { type: String, required: true }, 
   nivelDoMar: { type: Number, required: true },
-  valorAtual: { type: Number, required: true },
+  valorAtual: { type: String, required: true },
   valorPrevisto: Number,
   risco: String,
   imagem: String,
   linkLocalizacao: String
 });
+
 
 export default mongoose.model("Imovel", imovelSchema);
